ba260c06c836bde3b7b21329cf2f85a1a113aa97
denis <suddenig@gmail.com>
2026-02-14 23:37:24 +0100

    add api tests

---
+++ tests_api/test_login.py
@@ -0,0 +1,18 @@
+def test_login_success(client):
+    response = client.post(
+        "/login",
+        json={"username": "denis", "password": "1234"},
+    )
+    assert response.status_code == 203
+    data = response.json()
+    assert "access_token" in data
+    assert data["token_type"] == "bearer"
+
+
+def test_login_blocked_user(client):
+    response = client.post(
+        "/login",
+        json={"username": "blocked", "password": "1234"},
+    )
+    assert response.status_code == 403
+    assert response.json()["detail"]["code"] == "USER_BLOCKED"
